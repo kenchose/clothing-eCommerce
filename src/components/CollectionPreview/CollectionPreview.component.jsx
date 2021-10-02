a8824995e74810f85a7b3fb23bc5853801cd1e7d
Kenny Pham <kennypham@kennys-mbp-2.attlocal.net>
2021-10-01 18:02:15 -0700

    added CollectionItem and included it into CollectionPreview component.

--- src/components/CollectionPreview/CollectionPreview.component.jsx
+++ src/components/CollectionPreview/CollectionPreview.component.jsx
@@ -1,18 +1,33 @@
 import React from "react";
 
 import "./CollectionPreview.styles.scss";
+import CollectionItem from "../CollectionItem/CollectionItem.components";
 
 const CollectionPreview = ({ title, items }) => {
 	const itemPreview = () => {
 		return items
-			.filter((item, idx) => idx < 4)
-			.map((item) => <div key={item.id}>{item.name}</div>);
+			.slice(0, 4)
+			.map(({ id, ...otherItemProps }) => (
+				<CollectionItem key={id} {...otherItemProps}></CollectionItem>
+			));
+
+		// OR  //
+
+		// return (
+		// 	items
+		// 		.filter((item, idx) => idx < 4) // filter items to only 4 elements idx being second paramter (optional)
+		// 		// .map((item) => <div key={item.id}>{item.name}</div>); // map the 4 elements that were filtered
+		// 		.map(({ id, ...otherItemProps }) => (
+		// 			<CollectionItem key={id} {...otherItemProps}></CollectionItem>
+		// 		))
+		// );
 	};
 
 	return (
 		<div className="collection-preview">
 			<h1 className="title">{title.toUpperCase()}</h1>
 			<div className="preview">{itemPreview()}</div>
+			<CollectionItem />
 		</div>
 	);
 };
