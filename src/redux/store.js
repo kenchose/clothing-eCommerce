f11b8b605c84e2f98b5c1d1e1860353eb0aed23f
Kenny Pham <kennypham@kennys-mbp-2.attlocal.net>
2021-11-19 16:34:15 -0800

    redux-persist added using local storage

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,9 @@
 import { createStore, compose } from "redux";
+import { persistStore } from "redux-persist";
 
 import rootReducer from "./reducers";
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(rootReducer, composeEnhancers());
 
-export default store;
+export const store = createStore(rootReducer, composeEnhancers());
+export const persistor = persistStore(store);
