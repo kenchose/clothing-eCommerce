7feaa0c6fa37880466ab45024704a04b780dcb07
Kenny Pham <kennypham@kennys-mbp-2.attlocal.net>
2021-12-17 14:44:42 -0800

    Integrated styled-components into FormInput styles and can now create and save new collection with its documents

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -12,6 +12,8 @@ const config = {
 	measurementId: "G-FPY6WJN1PE",
 };
 
+firebase.initializeApp(config);
+
 export const createUserProfileDocument = async (userAuth, additionalData) => {
 	if (!userAuth) return;
 
@@ -37,7 +39,21 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
 	return userRef;
 };
 
-firebase.initializeApp(config);
+export const addCollectionAndDocuments = async (
+	collectionKey,
+	objectsToAdd
+) => {
+	const collectionRef = firestore.collection(collectionKey);
+	console.log(collectionRef);
+
+	const batch = firestore.batch();
+	objectsToAdd.forEach((obj) => {
+		const newDocRef = collectionRef.doc();
+		batch.set(newDocRef, obj);
+	});
+
+	return await batch.commit();
+};
 
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
